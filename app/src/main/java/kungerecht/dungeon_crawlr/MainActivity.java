8194ad9867ee54cfd6dc1a497bc5301a474ce480
kungerecht <kevinungerecht@gmail.com>
2018-08-08 03:24:30 -0700

    Initial Commit with new empty Android Project.

---
+++ app/src/main/java/kungerecht/dungeon_crawlr/MainActivity.java
@@ -0,0 +1,14 @@
+package kungerecht.dungeon_crawlr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Entry screen for game
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
